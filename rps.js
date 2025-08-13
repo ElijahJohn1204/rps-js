9b4863e5b35b10a1fb5a43edfb5b5d72f29effd2
ElijahJohn1204 <elijahtjohn@gmail.com>
2025-08-12 22:24:08 -0500

    Randomized Computer Output
    Used Math.random() and Math.floor() to generate the numbers 0, 1, or 2.
    Then used switch statement to assign them to corresponding choices.

--- rps.js
+++ rps.js
@@ -1,1 +1,11 @@
-console.log("Hello World")
+function getComputerChoice() {
+    choice = Math.floor(Math.random() * 3);
+    switch (choice) {
+        case 2:
+            return 'scissors';
+        case 1:
+            return 'paper';
+        default:
+            return 'rock';
+    }
+}
