795d23f92d7ef2fe3c410e977564d98791434ef9
ElijahJohn1204 <elijahtjohn@gmail.com>
2025-08-12 22:44:12 -0500

    Created PlayRound function
    Takes both user and computer choices as parameters and determines who won, sending a message to the console and incrementing the score.

--- rps.js
+++ rps.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     choice = Math.floor(Math.random() * 3);
     switch (choice) {
@@ -11,5 +14,21 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    return prompt("rock, paper, or scissors?")
+    return prompt("rock, paper, or scissors?");
+}
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+    if (humanChoice == computerChoice) {
+        console.log("Ah, we appear to have a tie! I suppose great minds think alike :|");
+    } else if ((computerChoice == 'rock' && humanChoice == 'scissors') || 
+               (computerChoice == 'paper' && humanChoice == 'rock') ||
+               (computerChoice == 'scissors' && humanChoice == 'paper')) {
+        console.log("I suppose no one ever taught you that " + computerChoice + " beats " + humanChoice + "!! Pitiful attempt, no mere mortal can defeat me!!");
+        computerScore++;
+    } else {
+        console.log("Egads!! You have had a stroke of luck. I hate to admit that " + humanChoice + " beats " + computerChoice + "?? Bet you won't play me again!")
+        humanScore++;
+    }
 }
