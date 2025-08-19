ad1f80068e8d968c0f0c73a7c4498caaa22dca2a
ElijahJohn1204 <elijahtjohn@gmail.com>
2025-08-18 22:11:55 -0500

    Added UI to RPS using DOM methods and buttons
    Cool to have the game be done entirely outside of the console. Nice to be able to give users more freedom.

--- rps.js
+++ rps.js
@@ -20,30 +20,56 @@ function getHumanChoice() {
 function playRound(humanChoice, computerChoice) {
     humanChoice = humanChoice.toLowerCase();
     computerChoice = computerChoice.toLowerCase();
+
+    result = document.querySelector(".result");
+    score = document.querySelector(".scoreboard");
     if (humanChoice == computerChoice) {
-        console.log("Ah, we appear to have a tie! I suppose great minds think alike :|");
+        result.textContent = "Ah, we appear to have a tie! I suppose great minds think alike :|";
     } else if ((computerChoice == 'rock' && humanChoice == 'scissors') || 
                (computerChoice == 'paper' && humanChoice == 'rock') ||
                (computerChoice == 'scissors' && humanChoice == 'paper')) {
-        console.log("I suppose no one ever taught you that " + computerChoice + " beats " + humanChoice + "!! Pitiful attempt, no mere mortal can defeat me!!");
+        result.textContent = "I suppose no one ever taught you that " + computerChoice + " beats " + humanChoice + "!! Pitiful attempt, no mere mortal can defeat me!!";
         computerScore++;
+        score.innerHTML = "Computer: " + computerScore + "<br>User: " + humanScore;
     } else {
-        console.log("Egads!! You have had a stroke of luck. I hate to admit that " + humanChoice + " beats " + computerChoice + " :(");
+        result.textContent = "Egads!! You have had a stroke of luck. I hate to admit that " + humanChoice + " beats " + computerChoice + " :(";
         humanScore++;
+        score.innerHTML = "Computer: " + computerScore + "<br>User: " + humanScore;
+    }
+
+    if (computerScore == 5) {
+        summary = document.querySelector(".summary");
+        summary.textContent = "I have defeated you by a score of " + computerScore + " to " + humanScore + "!! Can't say I'm surprised :D";
+    } 
+
+    if (humanScore == 5) {
+        summary = document.querySelector(".summary");
+        summary.textContent = "My word!! How can this be? You managed to beat me by a score of " + humanScore + " to " + computerScore + 
+                               "!! Don't get cocky, now!! Play me again or you're a big chicken >:("
     }
 }
 
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        humanChoice = getHumanChoice();
-        computerChoice = getComputerChoice();
-        playRound(humanChoice, computerChoice);
-    }
-    if (humanScore < computerScore) {
-        console.log("I have defeated you by a score of " + computerScore + " to " + humanScore + "!! Can't say I'm surprised :D");
-    } else if (humanScore == computerScore) {
-        console.log("How anticlimactic!! We each have a score of " + humanScore + "!! Dare you to play again >:)")
-    } else {
-        console.log("My word!! How can this be? You managed to beat me by a score of " + humanScore + " to " + computerScore + "!! Don't get cocky, now!! Be a man, and play me again >:(") 
-    }
-}
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        playRound(button.textContent, getComputerChoice());
+    });
+})
+
+
+// function playGame() {
+//     for (let i = 0; i < 5; i++) {
+//         humanChoice = getHumanChoice();
+//         computerChoice = getComputerChoice();
+//         playRound(humanChoice, computerChoice);
+//     }
+//     if (humanScore < computerScore) {
+//         console.log("I have defeated you by a score of " + computerScore + " to " + humanScore + "!! Can't say I'm surprised :D");
+//     } else if (humanScore == computerScore) {
+//         console.log("How anticlimactic!! We each have a score of " + humanScore + "!! Dare you to play again >:)");
+//     } else {
+//         console.log("My word!! How can this be? You managed to beat me by a score of " + humanScore + " to " + computerScore + 
+//                     "!! Don't get cocky, now!! Be a man, and play me again >:("); 
+//     }
+// }
